23de6afdc16f3ebc266b0cf52681e606708453f1
Ridho Muhamad <Ridho Muhamad>
2017-03-07 12:48:14 +0700

    recursive function

---
+++ cobaRecursive.js
@@ -0,0 +1,26 @@
+function numberSum(num) {
+  if(num == 1) {
+    return 1;
+  }
+  else {
+    return num * numberSum(num - 1);
+  }
+}
+console.log(numberSum(5)); // 5 + 4 + 3 + 2 + 1 = 15
+
+function factorial2(num1,num2){
+ if(num1 <= 1){
+   return 1;
+ }else {
+   var total = 1;
+   for(num1; num1 > 0 ; num1 -= num2){
+
+       total = total * num1;
+   }
+   return total;
+ }
+}
+
+console.log(factorial2(12,5));
+
+//thnx pak fadil :D :D
